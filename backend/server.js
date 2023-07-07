3e63cab6c475da0611308e9e47d750ff97b25da3
Shashi Kant Verma <vshashikant58@gmail.com>
2023-07-07 10:23:17 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is running sucessfully");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:_id", (req, res) => {
+  const singleChat = chats.find((c) => c.id === req.params.id);
+  res.send(singleChat);
+});
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, console.log(`Server is started on PORT ${PORT}`));
