bc4eea97b6c565c0af9f44a19cd17e1601222d80
Shashi Kant Verma <vshashikant58@gmail.com>
2023-07-07 12:12:33 +0530

    commited chatModel,messageModel,userModel

---
+++ backend/Models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+constUser = mongoose.model("User", UserSchema);
+module.exports = User;
