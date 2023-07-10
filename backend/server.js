3b11eb71224b8b820aaf9be7dca1f12b109fcf3c
Shashi Kant Verma <vshashikant58@gmail.com>
2023-07-10 10:41:50 +0530

    functions added

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,25 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
 dotenv.config();
 
 const app = express();
 
+app.use(express.json()); //to accept json data
+
 connectDB();
+
 app.get("/", (req, res) => {
   res.send("API is running sucessfully");
 });
+app.use(notFound);
+app.use(errorHandler);
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get("/api/chat/:_id", (req, res) => {
-  const singleChat = chats.find((c) => c.id === req.params.id);
-  res.send(singleChat);
-});
 const PORT = process.env.PORT || 3030;
+
 app.listen(PORT, console.log(`Server is started on PORT ${PORT}`.yellow.bold));
